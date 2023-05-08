e2e7f68a2dd39841508b626348cfac4c1aebf105
A.Bulgarsky <bulgarpepper@gmail.com>
2023-05-08 21:36:53 +0500

    new structure

---
+++ src/components/home/Slider.jsx
@@ -0,0 +1,76 @@
+import Carousel from 'react-bootstrap/Carousel';
+import React from 'react';
+import {Badge} from "react-bootstrap";
+
+import "../../css/Slider.css"
+
+function Slider() {
+    console.log("-> рендер Slider");
+    return (
+        <div class="slider">
+        <Carousel>
+            <Carousel.Item interval={1000}>
+                <img
+                    src="./img/surgut/1.jpg"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>Полотенца Philippus <Badge bg="dark" text="light">New</Badge>
+                    </h3>
+                    <p>Даже самый взыскательный покупатель будет приятно удивлен мягкостью и долговечностью данных полотенец.
+
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={5000}>
+                <img
+                    src="./img/surgut/2.jpg"
+                    alt="Second slide"
+                />
+                <Carousel.Caption>
+                    <h3>Полотенца Philippus <Badge bg="light" text='dark'>New</Badge></h3>
+                    <p>Решили перейти на штучный формат, вместо большой упаковки, так как он более востребован. .</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={5000}>
+                <img
+                    src="./img/surgut/3.jpg"
+                    alt="Third slide"
+                />
+                <Carousel.Caption>
+                    <h3>Полотенца для кухни Green Black <Badge bg="danger" text='light'>New</Badge> </h3>
+                    <p>
+                        В мае ожидаем поступление нового ассортимента.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={5000}>
+                <img
+                    src="./img/surgut/4.jpg"
+                    alt="Four slide"
+                />
+                <Carousel.Caption>
+                    <h3>Philippus Lux Cotton <Badge bg="primary" text='light'>New</Badge></h3>
+                    <p>
+                        Philippus производит полотенца, халаты, набедренные повязки, группы пике и покрывал с оригинальным подходом к производству.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item interval={5000}>
+                <img
+                     src="./img/surgut/5.jpg"
+                     alt="Four slide"
+                />
+                <Carousel.Caption>
+                    <h3>Качество упаковки <Badge bg="light" text='dark'>package</Badge></h3>
+                    <p>
+                        На фото полотенца Cestepe 70x140 упакованы в качественные ZIP пакеты, которые сохраняют форму и не мнутся. Имеют воздушный клапан, пакет герметичен, туда не попадает влага а полотенца дышат.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+        </div>
+    );
+}
+
+export default Slider;
---
+++ src/components/product/ProductList.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import ProductCard from "./ProductCard";
+import axios from 'axios';
+
+import "../../css/card.css"
+import {AppContext} from "../../App";
+
+const ProductList = (props) => {
+    console.log("JSON -> CardInfo");
+
+    const onAddOverlay = async (obj) => {
+        try{
+           const findOverlay = props.overalayItems.find(objOver => objOver.myId === obj.myId);
+           if(findOverlay) {
+               //await ?
+               await axios.delete(`https://64399fea90cd4ba563eae64a.mockapi.io/Cart/${findOverlay.id}`);
+               props.setOverlayItems((over) => over.filter(o => o.myId !== obj.myId))
+           }else{
+               const {data} = await axios.post('https://64399fea90cd4ba563eae64a.mockapi.io/Cart', obj);
+               props.setOverlayItems([...props.overalayItems, data]);
+           }
+        }
+        catch {
+            alert("Произошла ошибка при нажатии кнопки в Корзину");
+        }
+    }
+
+    console.log('рендер листа Products');
+
+    const onAddFavorite = async  (obj) => {
+        try{
+            const findFavorites = props.favorites.find(objFav => objFav.myId === obj.myId);
+            if(findFavorites) {
+                //await ?
+                await axios.delete(`https://64399fea90cd4ba563eae64a.mockapi.io/Favorites/${findFavorites.id}`);
+                props.setFavorites((over) => over.filter(o => o.myId !== obj.myId));
+            }else{
+                const {data} = await axios.post('https://64399fea90cd4ba563eae64a.mockapi.io/Favorites', obj);
+                props.setFavorites([...props.favorites, data]);
+            }
+        }
+        catch {
+            alert("Произошла ошибка при нажатии кнопки Избранное");
+        }
+    }
+
+    console.log(' -> рендер Продукт лист');
+
+    return (
+        <div class="card-container">
+            <h3>Our products:</h3>
+        <div class="card-box">
+            {
+            props.item.map(obj => {
+                return <ProductCard
+                    key={obj.id}
+                    id={obj.id}
+                    myId={obj.myId}
+                    title={obj.title}
+                    description={obj.description}
+                    price={obj.price}
+                    img={obj.img}
+
+                    onAddFavorite ={(favObj)=>onAddFavorite(favObj)}
+
+                    onAddBasket ={(cartObj)=>onAddOverlay(cartObj)}
+                    />
+                })
+            }
+        </div>
+        </div>
+    );
+};
+
+export default ProductList;
